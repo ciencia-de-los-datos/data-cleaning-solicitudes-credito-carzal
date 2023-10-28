e969fc4f07f0d0cbf8fe4dcd1a790cd2f79e6a5b
carzal <lmcaroz@unal.edu.co>
2023-10-28 10:30:13 -0500

    lab 5

--- pregunta.py
+++ pregunta.py
@@ -8,22 +8,40 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 """
 import pandas as pd
 from unidecode import unidecode
+import re
+
 
 def clean_data():
+    def format_date(str_date):
+        d = re.search(r'(^\d+)\/(\d+)\/(\d+)', str_date, re.IGNORECASE)
+        day = d.group(1)
+        month = d.group(2)
+        year = d.group(3)
+        if len(day)>2:
+            date = year + '/' + month + '/' + day
+            return date
+        else:
+            date = day + '/' + month + '/' + year
+            return date
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
-    df.rename(columns={'Unnamed: 0':'index'},inplace=True)
-    df.set_index('index',inplace=True)
-    
-    df["sexo"] = df["sexo"].apply(lambda x : x.upper())
-    df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].apply(lambda x : str(x).upper()).apply(lambda x: str(x).replace(" ","")).apply(lambda x: unidecode(x))
-    df["idea_negocio"] = df["idea_negocio"].apply(lambda x : str(x).upper()).apply(lambda x: str(x).replace(" ","")).apply(lambda x: unidecode(x)).apply(lambda x: str(x).replace("-","")).apply(lambda x: str(x).replace("_","")).apply(lambda x: str(x).replace("-","")).apply(lambda x: str(x).replace(".",""))
-    df["barrio"] = df["barrio"].apply(lambda x : str(x).upper()).apply(lambda x: str(x).replace(" ","")).apply(lambda x: unidecode(x)).apply(lambda x: str(x).replace("-","")).apply(lambda x: str(x).replace("_","")).apply(lambda x: str(x).replace("-","")).apply(lambda x: str(x).replace(".","")).apply(lambda x: str(x).replace("?","E"))
-    df["línea_credito"] = df["línea_credito"].apply(lambda x : str(x).upper()).apply(lambda x: str(x).replace(" ","")).apply(lambda x: unidecode(x)).apply(lambda x: str(x).replace("-","")).apply(lambda x: str(x).replace("_","")).apply(lambda x: str(x).replace("-","")).apply(lambda x: str(x).replace(".",""))
-    df["línea_credito"] = ["SOLIDARIA" if i == "SOLIDIARIA" else i for i in df["línea_credito"] ]
-    df["fecha_de_beneficio"] = pd.to_datetime(df["fecha_de_beneficio"], format= "mixed")
-    df["monto_del_credito"] = df["monto_del_credito"].apply(lambda x: x.replace("$","")).apply(lambda x: x.replace(" ","")).apply(lambda x: x.replace(",","")).astype(float).astype(int)
     df.drop_duplicates(inplace=True)
     df.dropna(inplace=True)
-    
+    df.rename(columns={'Unnamed: 0':'index'},inplace=True)
+    df.set_index('index',inplace=True)
+
+
+    df["sexo"] = df["sexo"].apply(lambda x : x.upper())
+    df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].apply(lambda x : str(x).upper()).apply(lambda x: str(x).replace(" ","")).apply(lambda x: unidecode(x)) 
+    df["idea_negocio"] = df["idea_negocio"].apply(lambda x : str(x).upper()).apply(lambda x: str(x).replace(" ","")).apply(lambda x: unidecode(x)).apply(lambda x: str(x).replace("-","")).apply(lambda x: str(x).replace("_","")).apply(lambda x: str(x).replace("-","")).apply(lambda x: str(x).replace(".",""))
+    #df["barrio"] = df["barrio"].apply(lambda x : str(x).upper()).apply(lambda x: str(x).replace(" ","")).apply(lambda x: unidecode(x)).apply(lambda x: str(x).replace("-","")).apply(lambda x: str(x).replace("_","")).apply(lambda x: str(x).replace("-","")).apply(lambda x: str(x).replace(".","")).apply(lambda x: str(x).replace("?","E"))
+    df.barrio = df.barrio.apply(lambda x : str(x).upper()).map(lambda x: re.sub("-| ", "_", str(x)))
+    df["línea_credito"] = df["línea_credito"].apply(lambda x : str(x).upper()).apply(lambda x: str(x).replace(" ","")).apply(lambda x: unidecode(x)).apply(lambda x: str(x).replace("-","")).apply(lambda x: str(x).replace("_","")).apply(lambda x: str(x).replace("-","")).apply(lambda x: str(x).replace(".",""))
+    #df["línea_credito"] = ["SOLIDARIA" if i == "SOLIDIARIA" else i for i in df["línea_credito"] ]
+    #df["fecha_de_beneficio"] = pd.to_datetime(df["fecha_de_beneficio"], format= "mixed")
+    df["monto_del_credito"] = df["monto_del_credito"].apply(lambda x: x.replace("$","")).apply(lambda x: x.replace(" ","")).apply(lambda x: x.replace(",","")).astype(float).astype(int)
+
+    df.drop_duplicates(inplace=True)
+    df.dropna(inplace=True)
+
     return df
